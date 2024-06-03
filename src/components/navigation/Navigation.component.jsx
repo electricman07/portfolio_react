99bd5cd235268b36af69c91cb957752077aeaa8e
gpopowic <gmpopowich@gmail.com>
2024-06-03 13:19:48 -0700

    added x animation for open close menu

--- src/components/navigation/Navigation.component.jsx
+++ src/components/navigation/Navigation.component.jsx
@@ -1,9 +1,25 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import Logo from "../../assets/images/Avatar50.png";
 import "./Navigation.styles.scss";
 
 const Navigation = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function mobileMenu() {
+    setShowMenu(!showMenu);
+  }
+
+  // Navbar scroll
+  const navbar = document.querySelector(".main-navbar");
+  window.onscroll = () => {
+    if (window.scrollY > 100) {
+      navbar.classList.add("nav-active");
+    } else {
+      navbar.classList.remove("nav-active");
+    }
+  };
+
   return (
     <Fragment>
       <nav className="main-navbar" id="navbar">
@@ -13,13 +29,16 @@ const Navigation = () => {
         </Link>
 
         <div className="nav-wrapper">
-          <div className="menu-icon">
-            <div className="bar1"></div>
-            <div className="bar2"></div>
-            <div className="bar3"></div>
+          <div
+            className={`menu-icon ${showMenu ? "nav-active" : ""}`}
+            onClick={mobileMenu}
+          >
+            <div className={`bar1 ${showMenu ? "bar1-active" : ""}`}></div>
+            <div className={`bar2 ${showMenu ? "bar2-active" : ""}`}></div>
+            <div className={`bar3 ${showMenu ? "bar3-active" : ""}`}></div>
           </div>
         </div>
-        <div className="nav-links-container">
+        <div className="nav-links-container" onClick={mobileMenu}>
           <Link className="nav-Link active" to="/">
             Home
           </Link>
